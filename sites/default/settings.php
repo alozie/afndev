f003f038a270fc98197e6ac4c3217aa062477dfc
Cash Williams <CashWilliams@gmail.com>
2015-05-05 16:45:29 -0700

    #36: Starting sites directory to support multisite

---
+++ sites/default/settings.php
@@ -0,0 +1,5 @@
+<?php
+
+// TODO: Set this to the proper Acquia subscription.
+$ac_client_id = 'default_example';
+require_once '../../conf/settings/base.settings.php';
---
+++ sites/mulisite.example.com/settings.php
@@ -0,0 +1,5 @@
+<?php
+
+// TODO: Set this to the proper Acquia subscription.
+$ac_client_id = 'multisite_example';
+require_once '../../conf/settings/base.settings.php';
