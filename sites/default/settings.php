f8d27336b3dfbfad43ae43ec88cdc9caa127ea43
Dane Powell <git@danepowell.com>
2015-05-05 17:02:55 -0700

    Dynamically choose base_url based on domain name.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,5 +1,5 @@
 <?php
 
 // TODO: Set this to the proper Acquia subscription.
-$ac_client_id = 'default_example';
+$ac_domain = 'default_example';
 require_once '../../conf/settings/base.settings.php';
