d1c1b56e2e56ba498e7cca06836851fba1938f3e
Daniel Montgomery <damontgomery@gmail.com>
2015-05-20 13:22:23 -0500

    added information around not using console.log()

--- docs/acquia_examples/js/acquia_examples.js
+++ docs/acquia_examples/js/acquia_examples.js
@@ -4,7 +4,10 @@
       // Get our setting from Drupal.settings
       var mySetting = settings.acquiaExamples;
 
-      console.log('Setting: ' + mySetting);
+      // We have console.log() here to make it easy to see that this code is functioning. You should never use console.log() on production code!
+      if (typeof console.log === 'function'){
+        console.log('Setting: ' + mySetting);
+      }
 
       // This will only ever get attached one time.
       $('#a-thing').once('acquiaExamples', function () {
