72626d54d3cc7f8d94951983767e3b685c6bc239
Matthew Grasmick <matthew.grasmick@acquia.com>
2015-09-30 11:10:47 -0400

    Fixing pt:create target and process.

--- task.sh
+++ task.sh
@@ -2,7 +2,9 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 if [ ! -f ${DIR}/bin/phing ]; then
-   echo "Phing was not found in this project's bin directory. Please run composer install."
+   echo "Phing was not found in this project's bin directory."
+   echo "Please run `composer install`."
+   echo "You may need to remove the `vendor` and `bin` directories first."
    exit 1
 fi
 
