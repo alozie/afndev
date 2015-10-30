0dde273eb8370db22880701adc549c604e0e6394
Matthew Grasmick <matthew.grasmick@acquia.com>
2015-10-30 11:01:47 -0400

    Adding namespace to database settings.

--- template/sites/default/settings/example.local.settings.php
+++ template/sites/default/settings/example.local.settings.php
@@ -14,6 +14,7 @@ if (empty($_ENV['AH_SITE_ENVIRONMENT'])) {
             'password' => '${db.password}',
             'host' => '${db.host}',
             'port' => '${db.port}',
+            'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
             'driver' => 'mysql',
             'prefix' => '',
           ),
@@ -21,12 +22,4 @@ if (empty($_ENV['AH_SITE_ENVIRONMENT'])) {
   );
 
   $base_url = '${project.local_url}';
-
-  // Disable virus scanning on uploaded files.
-  $conf['clamav_enabled'] = 0;
-  $conf['clamav_enable_element_image_widget'] = 0;
-  $conf['clamav_enable_element_file_widget'] = 0;
-  $conf['cron_safe_threshold'] = 0;
-
-  $conf['stage_file_proxy_origin'] = '';
 }
