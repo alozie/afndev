4c452cf8e6aba72cc47baf5380b06d2f127b9d73
Matthew Grasmick <matthew.grasmick@acquia.com>
2015-12-15 13:09:08 -0500

    Fixing Deploy tests in case upstream is up to date.

--- tests/phpunit/DeployTest.php
+++ tests/phpunit/DeployTest.php
@@ -70,10 +70,14 @@ class DeployTest extends \PHPUnit_Framework_TestCase
                 print "Executing \"$command\" \n";
                 $log .= shell_exec($command);
             }
+
             // We expect the remote git log to contain a commit message matching
-            // the syntax specified in deploy:build. I.e.,
+            // the current build number, unless this build has not introduced
+            // any new changes. Example message:
             // "Automated commit by Travis CI for Build #$travis_build_id".
-            $this->assertContains('#' . $_ENV['TRAVIS_BUILD_ID'], $log);
+            if (!empty($_ENV['DEPLOY_UPTODATE'])) {
+                $this->assertContains('#' . $_ENV['TRAVIS_BUILD_ID'], $log);
+            }
         }
     }
 }
