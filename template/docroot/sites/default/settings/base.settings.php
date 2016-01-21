0074fc507eb95aca0037bfa8e5bfd9ae8573c474
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-01-21 16:22:13 -0500

    Fixing free cloud tier detection.

--- template/docroot/sites/default/settings/base.settings.php
+++ template/docroot/sites/default/settings/base.settings.php
@@ -26,6 +26,6 @@ $ah_env = isset($_ENV['AH_SITE_ENVIRONMENT']) ? $_ENV['AH_SITE_ENVIRONMENT'] : N
 $is_ah_env = (bool) $ah_env;
 $is_ah_prod_env = ($ah_env == 'prod');
 $is_ah_stage_env = ($ah_env == 'test');
-$is_ah_free_tier = (!empty($_ENV['ACQUIA_HOSTING_DRUPAL_LOG']) && strstr($_ENV['ACQUIA_HOSTING_DRUPAL_LOG'], 'free'));
+$is_ah_dev_cloud = (!empty($_SERVER['HTTP_HOST']) && strstr($_SERVER['HTTP_HOST'], 'devcloud'));
 $is_ah_dev_env = (preg_match('/^dev[0-9]*$/', $ah_env) == TRUE);
 $is_local_env = !$is_ah_env;
