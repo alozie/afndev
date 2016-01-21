59fa0f6eab1a7f50ae0b2690e090a3036bda69e8
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-01-21 10:59:21 -0500

    Adding actual project prefix to git commit tests.

--- template/tests/phpunit/src/TestBase.php
+++ template/tests/phpunit/src/TestBase.php
@@ -1,11 +1,12 @@
 <?php
-
 /**
  * @file
  * Test base for project PHPUnit tests.
  */
 
 namespace Drupal\Tests\PHPUnit;
+
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class TestBase.
@@ -14,6 +15,21 @@ namespace Drupal\Tests\PHPUnit;
  */
 abstract class TestBase extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var string
+     *   The project root directory on the local machine.
+     */
+    protected $projectDirectory;
+    /**
+     * @var string
+     *   The docroot directory on the local machine.
+     */
+    protected $drupalRoot;
+    /**
+     * @var array
+     *   The yaml configuration from project.yml.
+     */
+    protected $config;
    /**
      * Class constructor.
      */
@@ -21,5 +37,6 @@ abstract class TestBase extends \PHPUnit_Framework_TestCase
     {
         $this->projectDirectory = dirname(dirname(dirname(__DIR__)));
         $this->drupalRoot = $this->projectDirectory . '/docroot';
+        $this->config = Yaml::parse(file_get_contents("{$this->projectDirectory}/project.yml"));
     }
 }
