dff7c0c2a180c74137e6d62976bf5406e7704c79
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-02-09 15:50:44 -0500

    Changing tests to use Drupal coding standards.

--- template/tests/behat/features/bootstrap/Drupal/FeatureContext.php
+++ template/tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -8,26 +8,20 @@
 namespace Drupal;
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
-use Drupal\DrupalExtension\Event\EntityEvent;
-use Drupal\Component\Utility\Random;
-use Behat\Behat\Context\BehatContext;
-use Behat\Behat\Context\Step;
-use Behat\Behat\Context\Step\Given;
-use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
+/**
+ * FeatureContext class defines custom step definitions for Behat.
+ */
+class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
+  /**
+   * Initializes context.
+   *
+   * Every scenario gets its own context instance.
+   * You can also pass arbitrary arguments to the
+   * context constructor through behat.yml.
+   */
+  public function __construct() {
 
-class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
-{
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
-    public function __construct()
-    {
-    }
+  }
+
 }
