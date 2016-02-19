fe52ed261cfa27104c8efa365fc3ed5aa513eea5
Jonathan Webb <jonathan.webb@acquia.com>
2016-02-19 15:19:21 -0500

    Adds clarity to including sensitive data in Drupal settings.

--- template/sites/default/settings.php
+++ template/sites/default/settings.php
@@ -480,9 +480,12 @@ require DRUPAL_ROOT . '/sites/default/settings/testing.settings.php';
  */
 if ($is_ah_env && file_exists('/var/www/site-php')) {
   require "/var/www/site-php/{$_ENV['AH_SITE_GROUP']}/{$_ENV['AH_SITE_GROUP']}-settings.inc";
-  $settings_file = '/mnt/gfs/' . $_ENV['AH_SITE_GROUP'] . '.' . $_ENV['AH_SITE_ENVIRONMENT'] . '/settings.php';
-  if (file_exists($settings_file)) {
-    require $settings_file;
+
+  // Store API Keys and things outside of version control.
+  // @see settings/sample-secrets.settings.php for sample code.
+  $secrets_file = sprintf('/mnt/gfs/%s.%s/secrets.settings.php', $_ENV['AH_SITE_GROUP'], $_ENV['AH_SITE_ENVIRONMENT']);
+  if (file_exists($secrets_file)) {
+    require $secrets_file;
   }
 }
 
