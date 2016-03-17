584957821e73da470668a5072dae6e7ad10b1012
Matthew Grasmick <matthew.grasmick@acquia.com>
2016-03-17 19:57:57 -0400

    Refactoring architecture to support multisite.

--- template/scripts/make/non-destructive-make-core.sh
+++ template/scripts/make/non-destructive-make-core.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+# This script will use drush make to download Drupal core and then rsync
+# specific files to a target directory. This allows drush make to be used
+# without completely destroying an existing docroot.
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 GIT_ROOT=$(git rev-parse --show-toplevel)
 DRUSH=$GIT_ROOT/vendor/bin/drush
@@ -7,6 +11,7 @@ DRUPAL_TEMP=$(mktemp -d)
 rm -rf $DRUPAL_TEMP
 
 $DRUSH make make.yml $DRUPAL_TEMP --quiet -y --projects=drupal --no-gitinfofile --concurrency=10
-rsync -a --no-g --no-p --delete --exclude-from=$DIR/rsync-exclude.txt --include=default.settings.php $DRUPAL_TEMP/* $GIT_ROOT/docroot
+rsync -a --no-g --no-p --delete --exclude-from=$DIR/rsync-exclude.txt --include-from=$DIR/rsync-include.txt $DRUPAL_TEMP/ $GIT_ROOT/docroot
+rsync -a --no-g --no-p --delete --ignore-existing --files-from=$DIR/rsync-include-if-does-not-exist.txt $DRUPAL_TEMP/ $GIT_ROOT/docroot
 
 rm -rf $DRUPAL_TEMP
